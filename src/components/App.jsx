183712f200e3176cd00e318dde31ad72d7aeed42
liussi <125599487+liussi@users.noreply.github.com>
2023-09-02 17:38:37 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,18 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <form action="">
+        <label htmlFor=""> Name
+          <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
+        </label>
+        <button>Add contact</button>
+      </form>
     </div>
   );
 };
