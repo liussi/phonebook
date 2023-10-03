3175c37d25fac49f1d72181097db6353ddfe41b6
liussi <125599487+liussi@users.noreply.github.com>
2023-10-03 23:35:22 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
-import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; 
+import {
+  persistStore, persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 const persistConfig = {
-  key: 'contactsList',
+  key: 'contactList',
   storage,
   blacklist: ['filter'],
 };
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-export const store = configureStore({reducer: persistedReducer });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
 
 export const persistor = persistStore(store);
+
