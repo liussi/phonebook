b7a281f0237a5b7598f017f57a433b82b037f0dd
liussi <125599487+liussi@users.noreply.github.com>
2023-10-03 20:32:04 +0300

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,26 +4,16 @@ import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Title } from './Global.style';
 import { useEffect, useMemo } from 'react';
-import store from '../store.js/store';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteContacts, addContacts } from '../store.js/contacts/contactSlise'
+import { deleteContacts, addContacts } from '../store.js/contacts/contactSlise';
 import { setFilterSearch } from 'store.js/filter/filterSlise';
 // import { addContacts, deleteContacts } from 'store.js/contacts/actions';
 
 export function App() {
-
   const { contacts } = useSelector(store => store.contacts);
- const {filter} = useSelector(store => store.filter);
-  console.log(filter);
-  console.log(contacts);
+  const { filter } = useSelector(store => store.filter);
 
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]);
-
-
 
   const onAddContact = newContact => {
     if (offAddContact(newContact.name)) {
@@ -40,20 +30,17 @@ export function App() {
     );
   };
 
-
   const onFilterSearch = e => {
     const value = e.target.value.toLowerCase();
     dispatch(setFilterSearch(value));
   };
 
-const getFilteredContacts = useMemo(() => {
-  const normalizedFilter = filter.toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
-}, [contacts, filter]);
-
-
+  const getFilteredContacts = useMemo(() => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }, [contacts, filter]);
 
   const deleteContact = contactId => {
     dispatch(deleteContacts(contactId));
@@ -62,10 +49,7 @@ const getFilteredContacts = useMemo(() => {
   return (
     <>
       <Title>Phonebook</Title>
-      <ContactForm
-        contacts={contacts}
-        onAddContact={onAddContact}
-      />
+      <ContactForm contacts={contacts} onAddContact={onAddContact} />
       <Title>Contacts</Title>
       <Filter filter={filter} onFilterSearch={onFilterSearch} />
       <ContactList
--- src/store.js/store.js
+++ src/store.js/store.js
@@ -1,4 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
 
-export const store = configureStore({reducer});
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist: ['filter'],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({reducer: persistedReducer });
+
+export const persistor = persistStore(store);
