a18295d389cace4ac387e2272016b147c0364b0f
liussi <125599487+liussi@users.noreply.github.com>
2023-10-07 09:32:26 +0300

    fix

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,6 @@
 import { Formik, useFormik } from 'formik';
 import { object, string } from 'yup';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addContacts } from 'redux/contacts/operations';
 import {
   FormWrapper,
@@ -9,6 +9,7 @@ import {
   FormButton,
   ErrorMessageForm,
 } from './Form.styled.js';
+import { getContacts } from 'redux/contacts/selector.js';
 
 const schema = object({
   name: string().min(2, 'Too Short!').max(70, 'Too Long!').required('Required'),
@@ -17,14 +18,24 @@ const schema = object({
 
 export default function ContactForm() {
   const dispatch = useDispatch();
+  const items = useSelector(getContacts);
 
+  const offAddContact = ({ name }) => {
+    return items.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+  };
   const formik = useFormik({
     initialValues: { name: '', phone: '' },
   });
 
-  const formHandleSubmit = (values,{resetForm}) => {
+  const formHandleSubmit = (values, { resetForm }) => {
     const { name, phone } = values;
 
+    if (offAddContact({ name })) {
+      alert('Contact with this name already exists!');
+      return;
+    }
     dispatch(addContacts({ name, phone }));
     resetForm();
   };
